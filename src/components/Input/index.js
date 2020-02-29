39c808ae5baf29e8b19e9d79af6264a66a20216b
Diana Ivanova <din.ivanova13@gmail.com>
2020-02-29 04:07:34 +0300

    feat: add input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./index.scss";
+
+export default class Input extends Component {
+    render() {
+        const { inputValue, onInputChange, onInputKeyPress } = this.props;
+
+        return (
+            <input
+                className="todo-input"
+                type="text"
+                placeholder="Write to add a task"
+                value={inputValue}
+                onChange={onInputChange}
+                onKeyPress={onInputKeyPress}
+            />
+        );
+    }
+}
