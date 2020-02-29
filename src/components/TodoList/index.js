d5695f7616a45f44daca6350d6b1a93f1719bf0a
Diana Ivanova <din.ivanova13@gmail.com>
2020-02-29 04:08:19 +0300

    feat: add todolist component

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./index.scss";
+import TodoItem from "../TodoItem";
+
+export default class TodoList extends Component {
+    render() {
+        const { todoList, onDeleteClick } = this.props;
+
+        return todoList.length ? (
+            <ul className="todo-list">
+                {todoList.map((todoItem, idx) => (
+                    <TodoItem
+                        key={idx}
+                        todoId={idx}
+                        todoItem={todoItem}
+                        onDeleteClick={onDeleteClick}
+                    />
+                ))}
+            </ul>
+        ) : (
+            <p className="todo-nothing">There is no any task to do</p>
+        );
+    }
+}
